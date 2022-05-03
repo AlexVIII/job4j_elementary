35f1dd01364e3be8cb1ecdac35fb99ea64eb1325
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-03 21:13:08 +0300

    1

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("С возвращением, Александр Васянин");
+        System.out.println("30 апреля 2022");
+    }
+}
