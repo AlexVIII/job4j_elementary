7d5e159e54535a6581ec44d061b98c69385e8503
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-03 21:14:41 +0300

    2

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         System.out.println("Сегодня 1 мая .");
-        System.out.println("Мир");
+        System.out.println("Мир ");
         System.out.println("   Труд");
         System.out.println("        Май");
         System.out.println("_____________");
--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("С возвращением, Александр Васянин");
-        System.out.println("30 апреля 2022");
+        System.out.println("30 апреля 2022 г");
     }
 }
