43b2f55433a093a19ec047fc8b915de0bfebaa8f
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-05 08:20:35 +0300

    4.2. Результат работы метода [#232622 #296377]

--- src/main/java/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -3,22 +3,18 @@ package ru.job4j.calculator;
 public class MathFunc {
 
     public static int func1(int x) {
-        int y;
-        y = x * x + 1;
-        return y;
-    }
+        return  x * x + 1;
+        }
 
     public static int func2(int x) {
-        int y;
-        y = 1 / x;
-        return y;
-    }
+        return  1 / x;
+          }
 
     public static void main(String[] args) {
        int result1 = MathFunc.func1(100);
        int result2 = MathFunc.func2(5);
        int result3 = result1 + result2;
-        System.out.println("Ответ: result = " + result3 );
+        System.out.println("Ответ: result = " + result3);
 
     }
 }
