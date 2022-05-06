dc2eba8e23541b49c288da21e19710c968c92708
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-06 06:34:43 +0300

    4.2. Результат работы метода [#232622 #296377]

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -3,11 +3,11 @@ package ru.job4j.calculator;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        return value / 60;
+        return value /70 ;
     }
 
     public static float rubleToDollar(float value) {
-        return value / 70;
+        return value / 60;
     }
 
     public static void main(String[] args) {
@@ -18,14 +18,3 @@ public class Converter {
         System.out.println(cash + " rubles are " + dollar + " dollar");
     }
 }
-
-//1. Реализовать метод конвертации рублей в евро и доллары.
-//
-//Вам нужно заменить -1 на формулу для вычисления валюты.
-//
-//1 доллар = 60 рублей.
-//1 евро = 70 рублей.
-//
-//Важное примечание: Здесь и далее по курсу в шаблонах могут присутствовать пояснительные комментарии в коде, их следует удалять. Надо запомнить, что комментарии в коде - плохой тон, а для документирования методов используется инструментарий JavaDoc. Он будет изучаться в следующих разделах курса.
-//
-//2. Доработайте метод main. Добавьте в него примеры выводов всех видов конвертаций.
