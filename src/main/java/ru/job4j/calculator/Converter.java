10b2a46044b7a82780ead3ae1c274c36aa73a612
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-07 14:24:31 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        return value / 70 ;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
