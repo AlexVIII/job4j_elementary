1384702939020840230d3da97d95b82a53049959
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-09 12:17:54 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/calculator/ConverterTest.java
+++ src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 public class ConverterTest {
 
     @Test
-    public void rubleToEuro() {
-        float cash = 140;
-        float euro = 2;
-        float cashToEuro = Converter.rubleToEuro(cash);
+    public void whenConvert140rubleToEuro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
         float eps = 0.0001f;
-     Assert.assertEquals(euro, cashToEuro, eps);
+     Assert.assertEquals(expected, out, eps);
     }
 
     @Test
-    public void rubleToDollar() {
-        float cash = 150;
-        float dollar = 2.5F;
-        float cashToDollar = Converter.rubleToDollar(cash);
+    public void whenConvert150rubleToDollar() {
+        float in = 150;
+        float expected = 2.5F;
+        float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
-        Assert.assertEquals(dollar, cashToDollar, eps);
+        Assert.assertEquals(expected, out, eps);
     }
 }
