3149ce0dcf08392361047669b79a2385bcfc651f
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-09 16:52:41 +0300

    0. Операции сравнения [#235631]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+     
+        int first = 5;
+        int second = 8;
+        boolean rsl;
+
+        rsl = first > second;
+        System.out.println(rsl);
+
+        rsl = first < second;
+        System.out.println(rsl);
+
+        rsl = first == second;
+        System.out.println(rsl);
+    }
+}
