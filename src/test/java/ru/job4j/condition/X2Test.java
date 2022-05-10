fd09c05998fbbc1dda7432a16f166f8d6589553d
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-10 12:29:45 +0300

    4. Тесты для стороны прямоугольника. [#159460]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -7,49 +7,45 @@ public class X2Test {
 
     @Test
     public void whenA1B1C1X1ThenRsl3() {
-        int a, b, c, x, expected, rsl;
-        a = 1;
-        b = 1;
-        c = 1;
-        x = 1;
-        expected = 3;
-        rsl = X2.calc(a, b, c, x);
+        int a = 1;
+        int b= 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 
     @Test
     public void whenA0B1C1X1ThenRsl2() {
-        int a, b, c, x, expected, rsl;
-        a = 0;
-        b = 1;
-        c = 1;
-        x = 1;
-        expected = 2;
-        rsl = X2.calc(a, b, c, x);
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 
     @Test
     public void whenA1B1C0X1ThenRsl2() {
-        int a, b, c, x, expected, rsl;
-        a = 1;
-        b = 1;
-        c = 0;
-        x = 1;
-        expected = 2;
-        rsl = X2.calc(a, b, c, x);
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 
     @Test
     public void whenA1B1C1X0ThenRsl1() {
-        int a, b, c, x, expected, rsl;
-        a = 1;
-        b = 1;
-        c = 1;
-        x = 0;
-        expected = 1;
-        rsl = X2.calc(a, b, c, x);
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 }
