bd0298f76f962f59f6202303467b33df9933f6b7
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-12 10:27:38 +0300

    4.3.2. Оператор || [#333784]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
-        boolean join = hasMoney || beFriend;
-        if (join == true) {
+        if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
         } else  {
             System.out.println("I can't.");
@@ -11,7 +10,7 @@ public class Club {
     }
 
     public static void main(String[] args) {
-       permission(true, true);
+        permission(true, true);
         permission(true, false);
         permission(false, true);
         permission(false, false);
