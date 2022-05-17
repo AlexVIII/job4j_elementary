e33b01c671a19b95e4cb4362dfbab29477440b24
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-17 06:34:50 +0300

    4.3.6. Math.abs [#373616]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
-        int rslX ;
-        int rslY ;
+        int rslX;
+        int rslY;
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
