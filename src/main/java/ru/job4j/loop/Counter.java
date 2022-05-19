7ee0e5a8298eb017794d55a5cd7c7f77c2eb00a7
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-19 06:12:35 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,9 +9,23 @@ public class Counter {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(1, 5));
-        System.out.println(sum(4, 9));
+    public static int sumByEvent(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
     }
+
+    public static void main(String[]args) {
+            System.out.println(sum(0, 10));
+            System.out.println(sum(1, 5));
+            System.out.println(sum(4, 4));
+            System.out.println(sumByEvent(0, 10));
+            System.out.println(sumByEvent(1, 5));
+            System.out.println(sumByEvent(4, 4));
+            System.out.println(sumByEvent(3, 3));
+        }
 }
