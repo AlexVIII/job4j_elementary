67e098ecd54a6a105178d3b95d8e4a0bdc2fc8ed
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-22 11:09:24 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+import java.rmi.AlreadyBoundException;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[5];
@@ -8,5 +10,12 @@ public class ArrayDefinition {
         System.out.println("Ages:" + ages.length);
         System.out.println("Surnames: "+ surnames.length);
         System.out.println("Prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Alex";
+        names[1] = "Bob";
+        names[2] = "Elena";
+        String name = names[0];
+        System.out.println(name);
+
     }
 }
