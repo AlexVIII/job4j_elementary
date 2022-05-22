851ba78bb1e7dd4bfda803d771f555eb60010324
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-22 11:23:08 +0300

    6.0.1.1. Размер массива [#333582 #303372]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,7 +8,7 @@ public class ArrayDefinition {
         String[] surnames = new String[100500];
         float[] prices = new float[40];
         System.out.println("Ages:" + ages.length);
-        System.out.println("Surnames: "+ surnames.length);
+        System.out.println("Surnames: " + surnames.length);
         System.out.println("Prices: " + prices.length);
         String[] names = new String[4];
         names[0] = "Alex";
