d1c59c94ecfe7cde92ab24907e313ced3566c7b7
AlexVIII <37580905+AlexVIII@users.noreply.github.com>
2022-05-23 18:27:18 +0300

    6.2.2. Переставить элементы массива [#226795]

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -8,8 +8,20 @@ public class SwitchArray {
         return array;
     }
 
+    public static int[] swap(int[] array, int source, int dest) {
+        int temp = array[source];
+        array[source] = array[dest];
+        array[dest] = temp;
+        return  array;
+    }
+//Вашей задачей является реализовать метод таким образом, чтобы значение ячеек с индексами source и dest поменялись местами. При этом:
+//
+//- source указывает на индекс элемента, который будет записан в ячейку с индексом dest;
+//
+//- dest указывает на индекс элемента, который будет записан в ячейку с индексом source;
+
     public static void main(String[] args) {
-        int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+        int[] nums = new int[] {10, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
         for (int index = 0; index < rsl.length; index++) {
             System.out.println(nums[index]);
---
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class SwitchArrayTest {
+
+    @Test
+    public void whenSwap1To4() {
+        int[] input = new int[] {1, 2, 3, 4, 5, 6};
+        int source = 1;
+        int dest = input.length - 2;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int [] expected = {1, 5, 3, 4, 2, 6};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int dest = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int[] expected = {4, 2, 3, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+}
